e10680e549e3e522678b716bd7ab870b122fe47a
KyryloPikhno <Mr.Good@i.ua>
2023-04-09 01:18:07 +0300

    added express

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
