86e1b271a95447224adb0b0d216acee2f7ee7981
KyryloPikhno <Mr.Good@i.ua>
2023-04-13 22:21:43 +0300

    added express

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const socketIO = require('socket.io');
 const mongoose = require('mongoose');
 const express = require('express');
 const http = require('http');
-
 require('dotenv').config();
 
 const app = express();
@@ -11,8 +10,13 @@ app.use(express.static('static'));
 
 const server = http.createServer(app);
 
-const io = socketIO(server);
+const io = socketIO(server, {cors: 'http://localhost:63342'});
 
 io.on('connection', (socket)=>{
     console.log(socket);
 })
+
+server.listen(process.env.PORT, async () => {
+    await mongoose.connect('mongodb://localhost:27017/simple-chat');
+    console.log(`Server listen ${process.env.PORT}`);
+});
