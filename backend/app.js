391ebcaf3c35c2e8dada94d1f1252520324c30b6
KyryloPikhno <Mr.Good@i.ua>
2023-04-13 22:15:09 +0300

    added express

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,18 @@
-const http = require('http');
 const socketIO = require('socket.io');
-const express = require('express');
 const mongoose = require('mongoose');
+const express = require('express');
+const http = require('http');
+
 require('dotenv').config();
 
+const app = express();
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('static'));
+
+const server = http.createServer(app);
+
+const io = socketIO(server);
+
+io.on('connection', (socket)=>{
+    console.log(socket);
+})
